dace7509be3ff997cf2546fb0445d0fe4afbe92f
Tom Bricaud <tom.bricaud1@orange.fr>
2024-02-01 17:51:27 +0100

    Use faster cartesian product implem using numpy

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,6 +1,8 @@
+import itertools
+import numpy as np
 import pytest
 
-from utils import result_iter_for_multiple_dices
+from utils import cartesian_product, result_iter_for_multiple_dices
 
 
 @pytest.mark.parametrize(
@@ -15,3 +17,17 @@ from utils import result_iter_for_multiple_dices
 )
 def test_result_list(number_of_dices, number_of_sides, output):
     assert list(result_iter_for_multiple_dices(number_of_dices, number_of_sides)) == output
+
+
+@pytest.mark.parametrize(
+    "number_of_arrays, array_size",
+    ((3, 2), (2, 3), (1, 3))
+)
+def test_cartesian_product(number_of_arrays, array_size):
+    arrays = [
+        np.array(range(10*i, 10*i + array_size))
+        for i in range(number_of_arrays)
+    ]
+    reference = [list(item) for item in itertools.product(*arrays)]
+
+    assert cartesian_product(*arrays).tolist() == reference
--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
 from collections import Counter
-import functools
+
 import numpy as np
-import itertools
 
 
-@functools.cache
 def probability_distribution(number_of_dices, number_of_sides):
     results = result_iter_for_multiple_dices(number_of_dices, number_of_sides)
     frequencies = Counter(results)
@@ -17,9 +15,21 @@ def probability_distribution(number_of_dices, number_of_sides):
 
 def result_iter_for_multiple_dices(number_of_dices, number_of_sides):
     result_list_for_one_dice = np.arange(1, number_of_sides + 1)
-    cartesian_product_of_results = np.array(list(
-        itertools.product(
-            result_list_for_one_dice, repeat=number_of_dices
-        )
-    ))
+    cartesian_product_of_results = cartesian_product(
+        *([result_list_for_one_dice] * number_of_dices)
+    )
     return np.sum(cartesian_product_of_results, axis=1)
+
+
+def cartesian_product(*arrays):
+    """Alternative to itertools.product.
+    
+    Copied from senderle's answer on
+    https://stackoverflow.com/questions/11144513/cartesian-product-of-x-and-y-array-points-into-single-array-of-2d-points
+    """
+    la = len(arrays)
+    dtype = np.result_type(*arrays)
+    arr = np.empty([len(a) for a in arrays] + [la], dtype=dtype)
+    for i, a in enumerate(np.ix_(*arrays)):
+        arr[...,i] = a
+    return arr.reshape(-1, la)
