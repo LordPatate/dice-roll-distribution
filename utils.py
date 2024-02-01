7d3b1c65d7f2d23c5af9c6fb24e158452fe4bc4c
Tom Bricaud <tom.bricaud1@orange.fr>
2024-02-01 10:14:21 +0100

    Utils: compute probability distribution

--- utils.py
+++ utils.py
@@ -1,8 +1,20 @@
+import functools
 import numpy as np
 import itertools
 
 
-def result_list_for_multiple_dices(numbers_of_dices, number_of_sides):
+@functools.cache
+def probability_distribution(number_of_dices, number_of_sides):
+    results = _result_list_for_multiple_dices(number_of_dices, number_of_sides)
+    frequencies = _frequency_map(results)
+    number_of_cases = number_of_sides**number_of_dices
+    return {
+        result: frequency / number_of_cases
+        for result, frequency in frequencies.items()
+    }
+
+
+def _result_list_for_multiple_dices(numbers_of_dices, number_of_sides):
     result_list_for_one_dice = np.arange(1, number_of_sides + 1)
     cartesian_product_of_results = itertools.product(
         *(
@@ -16,7 +28,7 @@ def result_list_for_multiple_dices(numbers_of_dices, number_of_sides):
     ]
 
 
-def frequency_map(result_list):
+def _frequency_map(result_list):
     freq_map = dict()
     for result in result_list:
         freq_map[result] = freq_map.get(result, 0) + 1
