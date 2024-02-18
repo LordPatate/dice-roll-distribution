c965ba94e62ed7a33e07faf3263a04ffcbe9175b
Tom Bricaud <tom.bricaud1@orange.fr>
2024-02-18 17:11:46 +0100

    Add labels to axes

--- src/render.py
+++ src/render.py
@@ -1,8 +1,13 @@
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
 
-def create_fig_from_distribution(distribution: dict):
+def create_fig_from_distribution(distribution: dict) -> Figure:
     x, y = list(zip(*distribution.items()))
+    ax: Axes
     fig, ax = plt.subplots()
     ax.bar(x, y)
+    ax.set_xlabel("Roll result")
+    ax.set_ylabel("Probability")
     return fig
